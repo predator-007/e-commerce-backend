36676290154c3556371668db150a50ef5ded3288
predator-007 <srijanraj2000@gmail.com>
2021-04-11 14:29:58 +0530

    commit

---
+++ index.js
@@ -0,0 +1,36 @@
+//express imports
+const express=require('express');
+const app=express();
+const verify=require('./Routes/verifytoken');
+
+
+//environment variables
+const dotenv=require('dotenv');
+dotenv.config();
+
+//mongodb connection
+const mongoose=require('mongoose');
+mongoose.connect(process.env.DB,
+{ useNewUrlParser: true }
+,()=>{
+    console.log("connected to database mongo");
+}
+);
+
+
+//authentication routes
+const authroutes= require('./Routes/auth.js');
+
+
+//middlewares
+app.use('/user',authroutes);
+app.use(express.json());
+
+
+//private
+app.get('/private',verify,(req,res)=>{
+
+    res.send("This is private route and you accessed it");
+});
+
+app.listen(process.env.PORT || 5000);
